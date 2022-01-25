d1633fa06c13957d53e5b21e378a17eb6ebb25a0
AmirMasoud Paydar <am.paydar@gmail.com>
2022-01-25 13:00:24 +0330

    add q6 solution

---
+++ q6-setare/src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n * 3 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= n + 1; i++) {
+
+            for (int j = 0; j < i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n * 3 + 1 - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int j = 0; j < i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n * 3 + 1 - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j < n * 3 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
+
